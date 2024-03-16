60c184ea6e3d85b572db4fe669b52271d8ba8b75
abojegabriel22 <abojejunior@gmail.com>
2024-03-16 12:29:58 +0100

    first angular from installing v15 and running first file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // template:`<h1>Hi from Aboje Gabriel</h1>`
+  templateUrl: './app.component.html'
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fullstack';
+  // title = 'fullstack';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Header/header.component';
 
 @NgModule({
+  // All the component and pipes
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
+  // All modules
   imports: [
-    BrowserModule,
-    AppRoutingModule
+      BrowserModule
+    // BrowserModule,
+    // AppRoutingModule
   ],
+  // All the services
   providers: [],
+
+
+  // only and only main component 
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
+
+// this was inside bootstrap 
+// 
